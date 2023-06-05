d3939122ef835ad305032efe8f311b376e84a6cb
Carlos Paqué <carlospaque95@gmail.com>
2023-06-05 16:13:50 +0200

    app base

---
+++ App.js
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import StackNavigator from './StackNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
