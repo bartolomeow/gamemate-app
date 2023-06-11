16593608a89ca1da540c32988f732eed505e212e
Carlos Paqué <carlospaque95@gmail.com>
2023-06-11 21:02:49 +0200

    feat(gamemate-app): implement google authenticator

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,16 +1,53 @@
-import React, { createContext, useContext } from "react";
-import * as Google from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
+WebBrowser.maybeCompleteAuthSession();
 
-    // const signInWithGoogle = async() => {
-    //     await Google.logInAsync()
-    // }
+const config = {
+    androidClientId:
+        "858333712885-t573fk6mkuumqpsn2f2kh8qph3vpe71q.apps.googleusercontent.com",
+    iosClientId:
+        "858333712885-tllmbbqlp1qt1ph7pg3vfsgu9j80peqr.apps.googleusercontent.com",
+    webClientId:
+        "858333712885-t573fk6mkuumqpsn2f2kh8qph3vpe71q.apps.googleusercontent.com",
+    scopes: ["profile", "email"],
+    permissions: ["public_profile", "email", "gender", "location"]
+};
+
+export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState("");
+    const [userInfo, setUserInfo] = useState(null);
+
+    const [request, response, signInWithGoogle] = Google.useAuthRequest(config);
+
+    useEffect(() => {
+        if (response?.type === "success") {
+            setToken(response.authentication.accessToken);
+            getUserInfo();
+        }
+    }, [response, token]);
+
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch(
+                "https://www.googleapis.com/userinfo/v2/me",
+                {
+                    headers: { Authorization: `Bearer ${token}` }
+                }
+            );
+
+            const user = await response.json();
+            setUserInfo(user);
+        } catch (error) {
+            // Add your own error handler here
+        }
+    };
 
     return (
-        <AuthContext.Provider value={{ user: null }}>
+        <AuthContext.Provider value={{ user: null, signInWithGoogle, request }}>
             {children}
         </AuthContext.Provider>
     );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
+    const { request, signInWithGoogle } = useAuth();
     return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
+        <>
+            <View>
+                <Text>LoginScreen</Text>
+            </View>
+            <Button
+                title="Sign in with Google"
+                disabled={!request}
+                onPress={signInWithGoogle}
+            />
+        </>
     );
 };
 
