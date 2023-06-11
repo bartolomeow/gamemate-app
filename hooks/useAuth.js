a7f4535de6bf5c2d7a70c9a9d770eea8a7c48599
Carlos Paqué <carlospaque95@gmail.com>
2023-06-11 22:52:47 +0200

    feat(gamemate-app): test google authenticator

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,6 +1,8 @@
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
+import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 const AuthContext = createContext({});
 
@@ -8,7 +10,7 @@ WebBrowser.maybeCompleteAuthSession();
 
 const config = {
     androidClientId:
-        "858333712885-t573fk6mkuumqpsn2f2kh8qph3vpe71q.apps.googleusercontent.com",
+        "858333712885-862edi68mjjp35phc9bm9ii8ig1ir995.apps.googleusercontent.com",
     iosClientId:
         "858333712885-tllmbbqlp1qt1ph7pg3vfsgu9j80peqr.apps.googleusercontent.com",
     webClientId:
@@ -18,33 +20,47 @@ const config = {
 };
 
 export const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState("");
-    const [userInfo, setUserInfo] = useState(null);
+    const [tokens, setTokens] = useState({
+        accessToken: undefined
+    });
+    // const [userInfo, setUserInfo] = useState(null);
+    const [error, setError] = useState(null);
 
     const [request, response, signInWithGoogle] = Google.useAuthRequest(config);
 
-    useEffect(() => {
-        if (response?.type === "success") {
-            setToken(response.authentication.accessToken);
-            getUserInfo();
-        }
-    }, [response, token]);
-
-    const getUserInfo = async () => {
+    useEffect(async () => {
         try {
-            const response = await fetch(
-                "https://www.googleapis.com/userinfo/v2/me",
-                {
-                    headers: { Authorization: `Bearer ${token}` }
-                }
-            );
+            if (response?.type === "success") {
+                const credential = await GoogleAuthProvider.credential(
+                    null,
+                    response.params.access_token
+                );
+                setTokens({
+                    accessToken: response.params.access_token
+                });
 
-            const user = await response.json();
-            setUserInfo(user);
+                await signInWithCredential(auth, credential);
+            }
         } catch (error) {
-            // Add your own error handler here
+            setError(error);
         }
-    };
+    }, [response]);
+
+    // const getUserInfo = async () => {
+    //     try {
+    //         const response = await fetch(
+    //             "https://www.googleapis.com/userinfo/v2/me",
+    //             {
+    //                 headers: { Authorization: `Bearer ${token}` }
+    //             }
+    //         );
+
+    //         const user = await response.json();
+    //         setUserInfo(user);
+    //     } catch (error) {
+    //         // Add your own error handler here
+    //     }
+    // };
 
     return (
         <AuthContext.Provider value={{ user: null, signInWithGoogle, request }}>
