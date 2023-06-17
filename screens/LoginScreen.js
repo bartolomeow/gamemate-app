5a95cec3d98d331a69994fcb15263fbb6165d1b6
Carlos Paqué <74964594+bartolomeow@users.noreply.github.com>
2023-06-17 10:56:09 +0200

    feat(gamemate-app): implement headersection (#3)

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,4 @@
-import { useNavigation } from "@react-navigation/native";
-import React, { useLayoutEffect } from "react";
+import React from "react";
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import { useTailwind } from "tailwind-rn";
 import useAuth from "../hooks/useAuth";
@@ -7,47 +6,31 @@ import useAuth from "../hooks/useAuth";
 const LoginScreen = () => {
     const tw = useTailwind();
     const { loading, signInWithGoogle } = useAuth();
-    const navigation = useNavigation();
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false
-        });
-    }, []);
 
     return (
-        <>
-            {/* <View>
-                <Text>{loading ? "Loading..." : "LoginScreen"}</Text>
-            </View>
-            <Button
-                title="Sign in with Google"
-                disabled={loading}
+        <View style={tw("flex-1")}>
+            <ImageBackground
+                resizeMode="cover"
+                style={tw("flex-1")}
+                source={{
+                    uri: "https://wallpaperaccess.com/full/649846.jpg"
+                }}
+            ></ImageBackground>
+            <TouchableOpacity
+                style={[
+                    tw("absolute bottom-40 w-52 bg-white p-4 rounded-2xl"),
+                    { marginHorizontal: "25%" }
+                ]}
                 onPress={signInWithGoogle}
-            /> */}
-            <View style={tw("flex-1")}>
-                <ImageBackground
-                    resizeMode="cover"
-                    style={tw("flex-1")}
-                    source={{
-                        uri: "https://wallpaperaccess.com/full/649846.jpg"
-                    }}
-                ></ImageBackground>
-                <TouchableOpacity
-                    style={[
-                        tw("absolute bottom-40 w-52 bg-white p-4 rounded-2xl"),
-                        { marginHorizontal: "25%" }
-                    ]}
-                    onPress={signInWithGoogle}
+            >
+                <Text
+                    style={tw("font-semibold text-center")}
+                    disabled={loading}
                 >
-                    <Text
-                        style={tw("font-semibold text-center")}
-                        disabled={loading}
-                    >
-                        Sign in & get swiping
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        </>
+                    Sign in & get swiping
+                </Text>
+            </TouchableOpacity>
+        </View>
     );
 };
 
