8d6c2e4b3dcd117f8f9b8b5bb264456923babd80
Carlos Paqué <74964594+bartolomeow@users.noreply.github.com>
2023-06-17 09:57:40 +0200

    Feature/register (#1)
    * feat(gamemate-app): prepare auth and gitignore
    
    * feat(gamemate-app): update package.json and useAuth
    
    * feat(gamemate-app): setup redirect scheme
    
    * feat(gamemate-app): change applicationid
    
    * feat(gamemate-app): comment loginasync
    
    * feat(gamemate-app): update packages
    
    * feat(gamemate-app): update gradle
    
    * feat(gamemate-app): update dependencies
    
    * feat(gamemate-app): update dependencies
    
    * feat(gamemate-app): implement google authenticator
    
    * feat(gamemate-app): test google authenticator
    
    * feat(gamemate-app): implement HOC
    
    * feat(gamemate-app): add tailwind properly
    
    * feat(gamemate-app): implement login screen

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,75 @@
-import React, { createContext, useContext } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import {
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithCredential,
+    signOut
+} from "firebase/auth";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 const AuthContext = createContext({});
 
+WebBrowser.maybeCompleteAuthSession();
+
+const config = {
+    androidClientId:
+        "858333712885-862edi68mjjp35phc9bm9ii8ig1ir995.apps.googleusercontent.com",
+    iosClientId:
+        "858333712885-tllmbbqlp1qt1ph7pg3vfsgu9j80peqr.apps.googleusercontent.com",
+    webClientId:
+        "858333712885-t573fk6mkuumqpsn2f2kh8qph3vpe71q.apps.googleusercontent.com",
+    scopes: ["profile", "email"],
+    permissions: ["public_profile", "email", "gender", "location"]
+};
+
 export const AuthProvider = ({ children }) => {
+    const [error, setError] = useState(null);
+    const [request, response, signInWithGoogle] = Google.useAuthRequest(config);
+    const [user, setUser] = useState(null);
+    const [loadingInitial, setLoadingInitial] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) setUser(user);
+            else setUser(null);
+            setLoadingInitial(false);
+            setLoading(false);
+        });
+    }, []);
+
+    const logout = () => {
+        setLoading(true);
+        signOut(auth)
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    };
+
+    useEffect(async () => {
+        setLoading(true);
+        try {
+            if (response?.type === "success") {
+                const credential = await GoogleAuthProvider.credential(
+                    null,
+                    response.params.access_token
+                );
+
+                await signInWithCredential(auth, credential);
+            }
+        } catch (error) {
+            setError(error);
+        } finally {
+            () => setLoading(false);
+        }
+    }, [response]);
+
     return (
-        <AuthContext.Provider value={{ user: "Sonny" }}>
-            {children}
+        <AuthContext.Provider
+            value={{ user, loading, error, logout, signInWithGoogle }}
+        >
+            {!loadingInitial && children}
         </AuthContext.Provider>
     );
 };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,53 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { useTailwind } from "tailwind-rn";
+import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
+    const tw = useTailwind();
+    const { loading, signInWithGoogle } = useAuth();
+    const navigation = useNavigation();
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        });
+    }, []);
+
     return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
+        <>
+            {/* <View>
+                <Text>{loading ? "Loading..." : "LoginScreen"}</Text>
+            </View>
+            <Button
+                title="Sign in with Google"
+                disabled={loading}
+                onPress={signInWithGoogle}
+            /> */}
+            <View style={tw("flex-1")}>
+                <ImageBackground
+                    resizeMode="cover"
+                    style={tw("flex-1")}
+                    source={{
+                        uri: "https://wallpaperaccess.com/full/649846.jpg"
+                    }}
+                ></ImageBackground>
+                <TouchableOpacity
+                    style={[
+                        tw("absolute bottom-40 w-52 bg-white p-4 rounded-2xl"),
+                        { marginHorizontal: "25%" }
+                    ]}
+                    onPress={signInWithGoogle}
+                >
+                    <Text
+                        style={tw("font-semibold text-center")}
+                        disabled={loading}
+                    >
+                        Sign in & get swiping
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </>
     );
 };
 
