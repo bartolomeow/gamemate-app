92a19adb1ed7014df7b11e1248bf44fa9293cfd7
Carlos Paqué <carlospaque95@gmail.com>
2023-06-24 11:50:44 +0200

    Update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-
-module.exports = getDefaultConfig(__dirname);
+const { getDefaultConfig } = require("expo/metro-config");
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
+exports.resolver = {
+    ...defaultResolver,
+    sourceExts: [...defaultResolver.sourceExts, "cjs"]
+};
